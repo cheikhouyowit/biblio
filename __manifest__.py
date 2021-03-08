f9212255c59b37eaec5784f3f721e033414b4216
root <cheikhousy@yowit.sn>
2021-03-08 15:44:51 -0500

    creation du module library et de la vue

--- __manifest__.py
+++ __manifest__.py
@@ -3,5 +3,6 @@
         'description': 'Gestion de la catalogue de livre d’une bibliothèque.',
         'author': 'Cheikhou',
         'depends': ['base'],
+        'data': ['views/library_menu.xml'],
         'application': True, 
     } 
