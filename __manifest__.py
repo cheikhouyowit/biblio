a77a5897b6892046f2dead4849a48f45a70f9ffb
root <cheikhousy@yowit.sn>
2021-03-08 14:32:00 -0500

    premier commit

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{
+        'name': 'Gestion bibliothèque',
+        'description': 'Gestion de la catalogue de livre d’une bibliothèque.',
+        'author': 'Cheikhou',
+        'depends': ['base'],
+        'application': True, 
+    } 
