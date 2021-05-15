66a26f02d9026761988f83d75aef342ff60d28a5
cheikhouyowit <cheikhousy@yowit.sn>
2021-05-15 06:45:18 +0000

    Update __manifest__.py
    update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,8 @@
 {
-        'name': 'Gestion bibliothèque',
-        'description': 'Gestion de la catalogue de livre d’une bibliothèque.',
-        'author': 'Cheikhou',
-        'depends': ['base'],
-        'data': ['views/library_menu.xml'],
-        'application': True, 
-    } 
+    'name': 'Gestion bibliothèque',
+    'description': 'Gestion de la catalogue de livre d’une bibliothèque.',
+    'author': 'Cheikhou',
+    'depends': ['base'],
+    'data': ['views/library_menu.xml'],
+    'application': True, 
+} 
