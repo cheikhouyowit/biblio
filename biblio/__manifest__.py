2728b67dd3dc229969b4722c5b05580c003f7dc4
cheikhouyowit <cheikhousy@yowit.sn>
2021-05-17 11:38:30 +0000

    ajout bouton check isbn

--- biblio/__manifest__.py
+++ biblio/__manifest__.py
@@ -5,8 +5,10 @@
     'depends': ['base'],
     'data': [
         'views/library_menu.xml',
+        'views/book_view.xml',
         'security/library_security.xml',
         'security/ir.model.access.csv',
+        
     ],
     'application' : True,
      
