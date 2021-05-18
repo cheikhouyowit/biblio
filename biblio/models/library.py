552d6bf3cfa852cf191a06e5a5863fd3d583a8e4
cheikhouyowit <cheikhousy@yowit.sn>
2021-05-18 12:13:21 +0000

    update controler

--- biblio/models/library.py
+++ biblio/models/library.py
@@ -9,8 +9,26 @@ class Book(models.Model):
 
 			
  
-
-	
+	@api.multi
+	def _check_isbn(self):
+		self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderators = [1, 3] * 6
+            total = sum(a * b for a, b in zip(digits[:12], ponderators))
+            remain = total % 10
+            check = 10 - remain if remain != 0 else 0
+    		return digits[-1] == check
+			
+	@api.multi
+	def button_check_isbn(self):
+		for book in self:
+			if not book.isbn:
+				raise Warning('Please provide an ISBN13 for %s' % book.name)
+			if book.isbn and not book._check_isbn():
+				raise Warning(
+                    '%s is an invalid ISBN' % book.isbn)
+		return True
 
 	name= fields.Char('Title', required=True) 
 	isbn= fields.Char('ISBN') 
