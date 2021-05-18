8eb61ea06f9b66e89e07c0078f95553d2c55d60a
cheikhouyowit <cheikhousy@yowit.sn>
2021-05-18 10:13:53 +0000

    controller et vue qweb

--- biblio/__manifest__.py
+++ biblio/__manifest__.py
@@ -6,6 +6,7 @@
     'data': [
         'views/library_menu.xml',
         'views/book_view.xml',
+        'views/book_list_template.xml',
         'security/library_security.xml',
         'security/ir.model.access.csv',
         
