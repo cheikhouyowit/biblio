b5dd0b441c5d43f248a3e13a3469fda1b0767393
cheikhouyowit <cheikhousy@yowit.sn>
2021-05-18 11:14:41 +0000

    Update library.py

--- biblio/models/library.py
+++ biblio/models/library.py
@@ -10,14 +10,7 @@ class Book(models.Model):
 			
  
 
-	@api.multi
-   	def button_check_isbn(self):
-		for book in self:
-			if not book.isbn:
-				raise Warning('Please provide an ISBN for %s' % book.name)
-			if book.isbn and not book._check_isbn():
-				   raise Warning('%s is an invalid ISBN' % book.isbn)
-		return True
+	
 
 	name= fields.Char('Title', required=True) 
 	isbn= fields.Char('ISBN') 
