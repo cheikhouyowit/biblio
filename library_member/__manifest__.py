d15dbb19c1951bad4796645533de8374ccd37350
cheikhouyowit <cheikhousy@yowit.sn>
2021-05-19 16:28:30 +0000

    Update __manifest__.py

--- library_member/__manifest__.py
+++ library_member/__manifest__.py
@@ -2,6 +2,10 @@
     'name': 'Library Members',
     'description': 'Manage people who will be able to borrow books.',
     'author': 'Daniel Reis',
+    'data': [
+        'views/book_view.xml',
+        
+    ],
     'depends': ['biblio'],
     'application': False,
 }
