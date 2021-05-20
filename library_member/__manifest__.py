e88e6dc9e3df5b997bf38d9b78bc083ad493757d
cheikhouyowit <cheikhousy@yowit.sn>
2021-05-20 13:04:45 +0000

    mise à jour model avec heritage

--- library_member/__manifest__.py
+++ library_member/__manifest__.py
@@ -4,6 +4,10 @@
     'author': 'Daniel Reis',
     'data': [
         'views/book_view.xml',
+        'views/member_view.xml',
+        'views/menu_member.xml',
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
         
     ],
     'depends': ['biblio'],
